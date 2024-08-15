1f14bb5261e233a5909be5b21cda2fd03f9bb14c
rers <rodisenrique73@gmail.com>
2024-08-15 09:02:42 -0500

    style: import styles

--- GA7-220501096-AA4-EV03/src/main.jsx
+++ GA7-220501096-AA4-EV03/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "animate.css";
+import "./index.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
