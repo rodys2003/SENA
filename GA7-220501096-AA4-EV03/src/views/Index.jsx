56cfb827266bda4cc4f783bb729adcf63be87ce8
rers <rodisenrique73@gmail.com>
2024-08-28 12:41:15 -0500

    add pagination and filter function

--- GA7-220501096-AA4-EV03/src/views/Index.jsx
+++ GA7-220501096-AA4-EV03/src/views/Index.jsx
@@ -9,27 +9,65 @@ import {
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { PokeTarjeta } from "../components/PokeTarjeta";
+import { PaginationControl } from "react-bootstrap-pagination-control";
 
 export function Index() {
-  const [pokemones, setPokemones] = useState([]);
-  const [offset, setoffset] = useState(0);
+  const [pokemones, setPokemones] = useState([]);  
+  const [allPokemones,setAllPokemones] = useState([]);
+  const [listado,setListado] = useState([]);
+  const [filtro,setFiltro] = useState('');
+  const [offset, setOffset] = useState(0);
   const [limit, setLimit] = useState(20);
+  const [total,setTotal] = useState(0);
 
   useEffect(() => {
     getPokemones(offset);
+    getAllPokemones();
   }, []);
 
   const getPokemones = async (o) => {
     const link =
-      "https://pokeapi.co/api/v2/pokemon?limit=" + limit + "&offset=" + offset;
+     "https://pokeapi.co/api/v2/pokemon?limit=" + limit + "&offset=" + o;
+    axios.get(link).then(async(response)=>{
+      const respuesta=response.data;
+      setPokemones(respuesta.results);
+      setListado(respuesta.results);
+      setTotal(respuesta.count)
+    })
+  }
 
-    axios.get(link).then(async (response) => {
-      const respuesta = response.data;
-      console.log(respuesta.results);
-    });
-  };
+  const getAllPokemones =async()=>{
+    const liga = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=100000';
+    axios.get(liga).then(async(response)=>{
+      const respuesta=response.data;
+      setAllPokemones(respuesta.results);    
+    })
+  }
 
-  return (
+  const buscar =async(e)=>{
+    if (e.keyCode==13){
+      if(filtro.trim()!=''){
+        setListado([]);
+        setTimeout(()=>{
+          setListado(allPokemones.filter(p=>p.name.includes(filtro)))
+        },100)
+      }
+
+    }else if(filtro.trim()==''){
+      setListado([]);
+      setTimeout(()=>{
+        setListado(pokemones)
+      },100)
+    }
+  }
+
+  const goPage =async(p)=>{
+    setListado([]);
+    await getPokemones((p==1) ?0:((p-1)*20));
+    setOffset(p);
+  }
+
+  return  (
     <Container className="shadow bg-dark mt-3 rounded">
       <Row>
         <Col>
@@ -37,15 +75,17 @@ export function Index() {
             <InputGroupText>
               <i className="fa-solid fa-search"></i>
             </InputGroupText>
-            <Input placeholder="Buscar pokemon"></Input>
+            <Input value={filtro} onChange={(e)=>{setFiltro(e.target.value)}} onKeyUpCapture={buscar} placeholder="Buscar pokemon"></Input>            
           </InputGroup>
         </Col>
       </Row>
       <Row className="mt-3">
-        {pokemones.map((pok, i) => {
-          <PokeTarjeta pok={pok} key={i} />;
-        })}
+        {listado.map((pok,i)=>(
+          <PokeTarjeta poke={pok} key={i}/>
+          
+        ))}
+        <PaginationControl last={true} limit={limit} total={total} page={offset} changePage={page=>goPage(page)} />
       </Row>
     </Container>
-  );    
+  );
 }
