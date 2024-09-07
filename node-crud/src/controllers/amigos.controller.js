fa90091e2e9f88e7d1a9f514e0f240656b4a9f2e
rers <rodisenrique73@gmail.com>
2024-09-07 13:27:56 -0500

    agregar metodos
    obtener por id, eliminar por id, actualizar y crear recurso

--- node-crud/src/controllers/amigos.controller.js
+++ node-crud/src/controllers/amigos.controller.js
@@ -12,6 +12,59 @@ const getFriends = async (req, res) => {
   }
 };
 
+const getFriend = async (req, res) => {
+  try {
+    const connection = await getConnection();
+    const { id } = req.params;
+    const result = await connection.query("select * from amigos where id = ?", [id]);
+    res.json(result[0]);
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
+};
+
+const addFriend = async (req, res) => {
+  try {
+    const connection = await getConnection();
+    const { nombre, telefono, facebook } = req.body;
+    await connection.query("insert into amigos (nombre, telefono, facebook) values (?, ?, ?)", [nombre, telefono,facebook]);
+    res.json({ nombre, telefono, facebook });
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
+};
+
+const updateFriend = async (req, res) => {
+  try {
+    const connection = await getConnection();
+    const { id } = req.params;
+    const { nombre, telefono, facebook } = req.body;
+    await connection.query("update amigos set nombre = ?, telefono = ?, facebook = ? where id = ?", [nombre, telefono, facebook, id]);
+    res.json({ nombre, telefono, facebook });
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
+};
+
+const deleteFriend = async (req, res) => {
+  try {
+    const connection = await getConnection();
+    const { id } = req.params;
+    await connection.query("delete from amigos where id = ?", [id]);
+    res.json({ id });
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
+};
+
 export const methods = {
   getFriends,
+  addFriend,
+  getFriend,
+  updateFriend,
+  deleteFriend
 };
