c83d36590c92961a75cf251e618110ac2a6608ba
rers <rodisenrique73@gmail.com>
2024-09-07 13:26:02 -0500

    agregar rutas
    ruta post, get por id, put y delete

--- node-crud/src/routes/amigos.route.js
+++ node-crud/src/routes/amigos.route.js
@@ -4,5 +4,9 @@ import { methods as amigosController } from "../controllers/amigos.controller";
 const router = Router();
 
 router.get("/", amigosController.getFriends);
+router.post("/", amigosController.addFriend);
+router.get("/:id", amigosController.getFriend);
+router.put("/:id", amigosController.updateFriend);
+router.delete("/:id", amigosController.deleteFriend);
 
 export default router;
