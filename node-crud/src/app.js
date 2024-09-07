18fa9feab340ce1345fed0630dcbdaff46de8c92
rers <rodisenrique73@gmail.com>
2024-09-07 12:40:45 -0500

    agregar ruta

--- node-crud/src/app.js
+++ node-crud/src/app.js
@@ -4,13 +4,13 @@ import morgan from "morgan";
 const app = express();
 
 // settings
-app.set("port",5000);
+app.set("port", 5000);
 // middlewares
 app.use(morgan("dev"));
 app.use(express.json());
 
 // routes
-
-app.get("/",(req,res)=>res.send("entro "));
+import amigosRoutes from "./routes/amigos.route";
+app.use("/api/friends", amigosRoutes);
 
 export default app;
